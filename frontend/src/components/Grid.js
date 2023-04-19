999a328876f37fbe8e7d50047a0dde9c0ffb8e62
daudahmad112 <94739129+daudahmad112@users.noreply.github.com>
2023-04-20 01:20:23 +0500

    added height width on 13,14
    added height width on 13,14
    line 7 "classname" to "className"

--- frontend/src/components/Grid.js
+++ frontend/src/components/Grid.js
@@ -4,12 +4,14 @@ const Grid = ({photos}) => {
   return (
     <>
         <h1>My Photo Gallery</h1>
-        <div classname = "grid">
+        <div className = "grid">
             {photos.map(({ photo, _id}) => (
                 <div key={_id} className='grid__item'>
                     <img
                         src={`https://photogallery-hgt4.onrender.com/${photo}`}
                         alt="grid_image"
+                        height="200"
+                        width="300"
                     />
                 </div>
             ))}
